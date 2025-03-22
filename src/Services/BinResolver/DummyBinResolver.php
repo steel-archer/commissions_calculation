98e97bbeda3722ecd752832f21772d831dbe60ec
Bohdan Sokulskyi <steelarcher@gmail.com>
2025-03-22 22:36:16 +0200

    Updated DummyBinResolver.

--- src/Services/BinResolver/DummyBinResolver.php
+++ src/Services/BinResolver/DummyBinResolver.php
@@ -2,14 +2,18 @@
 
 namespace SteelArcher\CommissionsCalculation\Services\BinResolver;
 
+use DomainException;
+
 class DummyBinResolver implements BinResolverInterface
 {
     public function getCountryAlpha2ByBin(string $bin): string
     {
-        // @todo add more
         return match ($bin) {
+            '516793', '4745030' => 'LT',
+            '41417360' => 'US',
+            '45417360' => 'JP',
             '45717360' => 'DK',
-            default => 'LT',
+            default => throw new DomainException("Can't get country by bin: $bin."),
         };
     }
 }
